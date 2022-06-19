aa8750b7fd91c1438b928cc0a519ff78508b0406
tonnyhawk <tonnyhawk93@gmail.com>
2022-06-19 20:57:07 +0300

    add dockerfile

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'front/build')));
 
